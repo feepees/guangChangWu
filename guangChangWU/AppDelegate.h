27331636ff44c355ffa586eda65d3a25e2246cc0
feepees <feepees_lee@163.com>
2017-05-23 09:31:11 +0800

    Initial Commit

---
+++ guangChangWU/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  guangChangWU
+//
+//  Created by Android on 2017/5/23.
+//  Copyright © 2017年 cc.youdu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
